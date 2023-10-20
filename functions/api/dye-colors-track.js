f5c451c341018168e5924877b4325db84c71bc0f
Snowy <snowyyang@outlook.com>
2023-10-20 11:30:03 +0800

    begin test api

--- functions/api/dye-colors-track.js
+++ functions/api/dye-colors-track.js
@@ -4,6 +4,6 @@ export async function onRequestPost(context) {
       var color = formData.get('color');
       var more = formData.get('more');
       var ip = context.request.headers.get('CF-Connecting-IP');
-      db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip));
+      //db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip));
   });
 }
