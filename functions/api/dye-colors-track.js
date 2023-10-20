dd365ac84d813f9803c323c835eea680f39cc45a
Snowy <snowyyang@outlook.com>
2023-10-20 11:41:58 +0800

    exec to run

--- functions/api/dye-colors-track.js
+++ functions/api/dye-colors-track.js
@@ -1,5 +1,5 @@
 export async function onRequestPost(context) {
-  context.env.db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip));
+  context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind("#000",0,"127.0.0.1").run();
   /*context.request.formData()
   .then((formData)=>{
       var color = formData.get('color');
