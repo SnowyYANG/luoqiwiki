ce81c6bc9c108c03946b4fd5dccc1bcb434d910a
Snowy <snowyyang@outlook.com>
2023-10-20 12:03:13 +0800

    then to await

--- functions/api/dye-colors-track.js
+++ functions/api/dye-colors-track.js
@@ -1,10 +1,8 @@
 export async function onRequestPost(context) {
-  context.request.formData()
-  .then((formData)=>async function() {
-      var color = formData.get('color');
-      var more = formData.get('more');
-      var ip = context.request.headers.get('CF-Connecting-IP');
-      await context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip).run();
-  });
+  const formData = await context.request.formData();
+  var color = formData.get('color');
+  var more = formData.get('more');
+  var ip = context.request.headers.get('CF-Connecting-IP');
+  await context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip).run();
   return new Response("Hello, world!");
 }
