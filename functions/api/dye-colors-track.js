9b065601ba1721216196cbf25ecfd707701b4458
Snowy <snowyyang@outlook.com>
2023-10-20 11:54:10 +0800

    full

--- functions/api/dye-colors-track.js
+++ functions/api/dye-colors-track.js
@@ -1,11 +1,10 @@
 export async function onRequestPost(context) {
-  await context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind("#000",0,"127.0.0.1").run();
-  return new Response("Hello, world!");
-  /*context.request.formData()
+  context.request.formData()
   .then((formData)=>{
       var color = formData.get('color');
       var more = formData.get('more');
       var ip = context.request.headers.get('CF-Connecting-IP');
-      //context.env.db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip));
-  });*/
+      context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip).run();
+  });
+  return new Response("Hello, world!");
 }
