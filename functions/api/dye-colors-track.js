405b9626fae509becf1501afed4364e05f3030fe
Snowy <snowyyang@outlook.com>
2023-10-20 11:38:10 +0800

    fix db

--- functions/api/dye-colors-track.js
+++ functions/api/dye-colors-track.js
@@ -1,10 +1,10 @@
 export async function onRequestPost(context) {
-  db.exec('INSERT INTO `dye-colors-search` (color,more,ip) VALUES("000",0,"127.0.0.1")');
+  context.env.db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip));
   /*context.request.formData()
   .then((formData)=>{
       var color = formData.get('color');
       var more = formData.get('more');
       var ip = context.request.headers.get('CF-Connecting-IP');
-      //db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip));
+      //context.env.db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip));
   });*/
 }
