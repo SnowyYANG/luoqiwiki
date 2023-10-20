7d701c22827f19be96868ea96fae4d5084785fc5
Snowy <snowyyang@outlook.com>
2023-10-20 11:34:07 +0800

    comment formdata

--- functions/api/dye-colors-track.js
+++ functions/api/dye-colors-track.js
@@ -1,9 +1,10 @@
 export async function onRequestPost(context) {
-  context.request.formData()
+  db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind("000",0,"127.0.0.1"));
+  /*context.request.formData()
   .then((formData)=>{
       var color = formData.get('color');
       var more = formData.get('more');
       var ip = context.request.headers.get('CF-Connecting-IP');
       //db.exec(context.env.db.prepare('INSERT INTO `dye-colors-search` (color,more,ip) VALUES(?,?,?)').bind(color,more,ip));
-  });
+  });*/
 }
